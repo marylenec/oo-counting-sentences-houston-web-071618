68d49815476aa5a25ceea16f8748173fac4b19fd
Marylene Chan <marylenechan@marylenes-mbp-2.attlocal.net>
2018-07-19 21:28:10 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    elements = self.split(/\.|\?|\!/)
+    elements.delete_if {| s | s == ""}
 
+    count = elements.length
+    # binding.pry
   end
+
 end
